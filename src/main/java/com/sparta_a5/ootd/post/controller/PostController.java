5098fd85f5af055efba9ce59805cf53864bf862b
Yoon <dlstjr9390@gmail.com>
2023-12-06 20:02:28 +0900

    feat : ResponseEntity 반환 및 게시글 삭제 기능

--- src/main/java/com/sparta_a5/ootd/post/controller/PostController.java
+++ src/main/java/com/sparta_a5/ootd/post/controller/PostController.java
@@ -5,6 +5,7 @@ import com.sparta_a5.ootd.post.dto.PostResponseDto;
 import com.sparta_a5.ootd.post.service.PostService;
 import com.sparta_a5.ootd.user.entity.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,22 +18,33 @@ public class PostController {
     private final PostService postService;
 
     @PostMapping("")
-    public PostResponseDto createPost(@RequestBody PostRequestDto postRequestDto){
-        return postService.createPost(postRequestDto);
+    public ResponseEntity<PostResponseDto> createPost(@RequestBody PostRequestDto postRequestDto){
+        PostResponseDto postResponseDto = postService.createPost(postRequestDto);
+        return ResponseEntity.ok(postResponseDto);
     }
 
     @GetMapping("")
-    public List<PostResponseDto> getPostList(){
-        return postService.getPostList();
+    public ResponseEntity<List<PostResponseDto>> getPostList(){
+        List<PostResponseDto> postResponseDtoList = postService.getPostList();
+        return ResponseEntity.ok(postResponseDtoList);
     }
 
     @GetMapping("/{postId}")
-    public PostResponseDto getPost(@PathVariable Long postId){
-        return postService.getPost(postId);
+    public ResponseEntity<PostResponseDto> getPost(@PathVariable Long postId){
+        PostResponseDto postResponseDto = postService.getPost(postId);
+        return ResponseEntity.ok(postResponseDto);
     }
 
     @PatchMapping("/{postId}")
-    public PostResponseDto updatePost(@PathVariable Long postId, @RequestBody PostRequestDto postRequestDto) {
-        return postService.updatePost(postId, postRequestDto);
+    public ResponseEntity<PostResponseDto> updatePost(@PathVariable Long postId, @RequestBody PostRequestDto postRequestDto) {
+        PostResponseDto postResponseDto = postService.updatePost(postId, postRequestDto);
+        return ResponseEntity.ok(postResponseDto);
+    }
+
+    @DeleteMapping("/{postId}")
+    public ResponseEntity<List<PostResponseDto>> deletePost(@PathVariable Long postId){
+        postService.deletePost(postId);
+        List<PostResponseDto> postResponseDtoList = postService.getPostList();
+        return ResponseEntity.ok(postResponseDtoList);
     }
 }
