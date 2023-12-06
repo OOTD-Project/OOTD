9d9b72cbc0c7e8794963df0d8fa4ecdc4d52cae1
Yoon <dlstjr9390@gmail.com>
2023-12-06 17:33:52 +0900

    feat: 기본 코드 추가

--- src/main/java/com/sparta_a5/ootd/post/entity/Post.java
+++ src/main/java/com/sparta_a5/ootd/post/entity/Post.java
@@ -1,5 +1,7 @@
 package com.sparta_a5.ootd.post.entity;
 
+import com.sparta_a5.ootd.post.dto.PostRequestDto;
+import com.sparta_a5.ootd.post.timestamped.Timestamped;
 import com.sparta_a5.ootd.user.entity.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
@@ -9,7 +11,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Post {
+public class Post extends Timestamped {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -18,7 +20,22 @@ public class Post {
     @JoinColumn(name = "user_id")
     private User user;
 
+    @Column(nullable = false)
     private String title;
+    @Column(nullable = false)
     private String content;
+    @Column(nullable = false)
     private String imageURL;
+
+    public Post(PostRequestDto postRequestDto){
+        this.title = postRequestDto.getTitle();
+        this.content = postRequestDto.getContent();
+        this.imageURL = postRequestDto.getImageURL();
+    }
+
+    public void update(PostRequestDto postRequestDto) {
+        this.title = postRequestDto.getTitle();
+        this.content = postRequestDto.getContent();
+        this.imageURL = postRequestDto.getImageURL();
+    }
 }
