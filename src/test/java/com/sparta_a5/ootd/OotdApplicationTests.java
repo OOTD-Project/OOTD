1485360e8d09ebf5942577cabd7f8170761e10fb
zzzzseong <jung48182@gmail.com>
2023-12-06 10:18:25 +0900

    initial commit

---
+++ src/test/java/com/sparta_a5/ootd/OotdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta_a5.ootd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OotdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
