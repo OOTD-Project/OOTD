fb21bf1bf93b07480cc41436191434d24c5caef2
zzzzseong <jung48182@gmail.com>
2023-12-06 10:31:30 +0900

    feat(user,post,comment): 기본 엔티티 구성

---
+++ src/main/java/com/sparta_a5/ootd/comment/entity/Comment.java
@@ -0,0 +1,27 @@
+package com.sparta_a5.ootd.comment.entity;
+
+import com.sparta_a5.ootd.post.entity.Post;
+import com.sparta_a5.ootd.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    private String comment;
+}
