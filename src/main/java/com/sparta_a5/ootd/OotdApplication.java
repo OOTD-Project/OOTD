98ffae25c1242f2190c7cf9dc8a880e976e2419c
Yoon <dlstjr9390@gmail.com>
2023-12-06 20:28:16 +0900

    feat : @EnableJpaAuditing 추가

--- src/main/java/com/sparta_a5/ootd/OotdApplication.java
+++ src/main/java/com/sparta_a5/ootd/OotdApplication.java
@@ -2,7 +2,9 @@ package com.sparta_a5.ootd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class OotdApplication {
 
