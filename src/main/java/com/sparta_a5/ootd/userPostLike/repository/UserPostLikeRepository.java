351d4f239eefd90832b26b05bcc1b424d554dfc7
Yoon <dlstjr9390@gmail.com>
2023-12-06 20:06:00 +0900

    feat : 게시글 좋아요 리포지토리

---
+++ src/main/java/com/sparta_a5/ootd/userPostLike/repository/UserPostLikeRepository.java
@@ -0,0 +1,7 @@
+package com.sparta_a5.ootd.userPostLike.repository;
+
+import com.sparta_a5.ootd.userPostLike.entity.UserPostLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserPostLikeRepository extends JpaRepository<UserPostLike, Long> {
+}
