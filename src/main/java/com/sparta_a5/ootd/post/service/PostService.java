f4f69d9078e97d3d1b3832927dcab16bb859d00c
Yoon <dlstjr9390@gmail.com>
2023-12-06 17:41:21 +0900

    feat: 게시글 작성, 조회, 수정 (예외처리 , 인가 제외)

---
+++ src/main/java/com/sparta_a5/ootd/post/service/PostService.java
@@ -0,0 +1,49 @@
+package com.sparta_a5.ootd.post.service;
+
+import com.sparta_a5.ootd.post.dto.PostRequestDto;
+import com.sparta_a5.ootd.post.dto.PostResponseDto;
+import com.sparta_a5.ootd.post.entity.Post;
+import com.sparta_a5.ootd.post.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+
+    private final PostRepository postRepository;
+    public PostResponseDto createPost(PostRequestDto postRequestDto) {
+        Post post = postRepository.save(new Post(postRequestDto));
+        return new PostResponseDto(post);
+    }
+
+    public List<PostResponseDto> getPostList() {
+        List<Post> postList = postRepository.findAll();
+        List<PostResponseDto> postResponseDtoList = postList.stream()
+                .map(PostResponseDto::new)
+                .toList();
+        return postResponseDtoList;
+
+    }
+
+    public PostResponseDto getPost(Long postId) {
+        Post post = postRepository.findById(postId).orElseThrow(
+                ()-> new IllegalArgumentException("존재하지 않는 글입니다.")
+        );
+        return new PostResponseDto(post);
+    }
+
+    @Transactional
+    public PostResponseDto updatePost(Long postId, PostRequestDto postRequestDto) {
+         Post post = postRepository.findById(postId).orElseThrow(
+                 ()-> new IllegalArgumentException("존재하지 않는 글입니다.")
+         );
+
+         post.update(postRequestDto);
+         return new PostResponseDto(post);
+    }
+
+}
