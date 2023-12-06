f4a057e5689276d1754b9200fdea17d4b8322075
Yoon <dlstjr9390@gmail.com>
2023-12-06 20:10:33 +0900

    feat : 게시물 좋아요 서비스(미구현)

---
+++ src/main/java/com/sparta_a5/ootd/userPostLike/service/UserPostLikeService.java
@@ -0,0 +1,11 @@
+package com.sparta_a5.ootd.userPostLike.service;
+
+import com.sparta_a5.ootd.post.entity.Post;
+import com.sparta_a5.ootd.user.entity.User;
+
+public class UserPostLikeService {
+    public void clickPostLike(Long Id){
+
+    }
+
+}
