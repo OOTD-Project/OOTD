f91b9adf282a5ca84351716814376739d788667c
Yoon <dlstjr9390@gmail.com>
2023-12-07 15:38:42 +0900

    feat  : 이미지URL 멀티파일이름으로 수정

--- src/main/java/com/sparta_a5/ootd/post/dto/PostRequestDto.java
+++ src/main/java/com/sparta_a5/ootd/post/dto/PostRequestDto.java
@@ -2,6 +2,7 @@ package com.sparta_a5.ootd.post.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 @Setter
@@ -9,5 +10,5 @@ public class PostRequestDto {
 
     private String title;
     private String content;
-    private String imageURL;
+    private MultipartFile filename;
 }
