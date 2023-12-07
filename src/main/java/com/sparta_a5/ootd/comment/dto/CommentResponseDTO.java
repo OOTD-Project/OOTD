8b07528e90405970187b0be2053ee6a3986512dd
김은채 <ke808762@gmail.com>
2023-12-07 18:14:52 +0900

    feat(comment) : created_at 추가

--- src/main/java/com/sparta_a5/ootd/comment/dto/CommentResponseDTO.java
+++ src/main/java/com/sparta_a5/ootd/comment/dto/CommentResponseDTO.java
@@ -5,14 +5,20 @@ import com.sparta_a5.ootd.comment.entity.Comment;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 public class CommentResponseDTO extends CommonResponseDTO {
     private Long id;
     private String comment;
 
+    // created_at 추가
+    private LocalDateTime created_at;
+
     public CommentResponseDTO(Comment comment) {
         this.id = comment.getId();
         this.comment = comment.getComment();
+        this.created_at = comment.getCreated_at(); // created_at 추가
     }
 }
