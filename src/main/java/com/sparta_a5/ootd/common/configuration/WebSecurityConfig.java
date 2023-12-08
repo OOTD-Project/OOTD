7602c6eed75f7c25f9e2fd4e3b274c25565d787c
zzzzseong <jung48182@gmail.com>
2023-12-08 16:02:22 +0900

    fix(follow): query repository 오류 해결

--- src/main/java/com/sparta_a5/ootd/common/configuration/WebSecurityConfig.java
+++ src/main/java/com/sparta_a5/ootd/common/configuration/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import com.sparta_a5.ootd.user.security.UserDetailsServiceImpl;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
